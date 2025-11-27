e4375df49d70e05675449f5fc3fa689fa7b59186
Karol0328 <bill19990328@gmail.com>
2025-11-27 08:07:16 +0800

    Rename import from EtfTracker to LiquidationFeed

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { SpotMarket } from './pages/SpotMarket';
 import { FuturesDashboard } from './pages/FuturesDashboard';
-import { EtfTracker } from './pages/LiquidationFeed';
+import { LiquidationFeed } from './pages/LiquidationFeed';
 import { MarketTab } from './types';
 
 function App() {
